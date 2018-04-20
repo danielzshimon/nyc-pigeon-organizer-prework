d21c50ba2b8367ed82ceab70b2dac017bfb7c514
Daniel Shimon <danielzshimon@gmail.com>
2018-04-20 15:04:20 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   new_pigeon = {}
-  pigeon_data.each_pair do |key, value|
+  data.each_pair do |key, value|
      value.each_pair do |attribute, name_array|
         name_array.each do |name|
           if new_pigeon[name].nil?
